f95aa7282c44ea302e706f2be8a72b6e2e42b3a3
Ashley <ashleydai2003@gmail.com>
2025-04-14 14:04:39 -0700

    Actual Initial Commit

---
+++ Cove/Cove/CoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoveApp.swift
+//  Cove
+//
+//  Created by Ashley Dai on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
