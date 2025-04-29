8fd175042cfa896d2a01e2baade6b7d5dae8f97c
Ashley <ashleydai2003@gmail.com>
2025-04-29 00:29:57 -0700

    Backend changes for SDK

--- Infra/iam.tf
+++ Infra/iam.tf
@@ -48,7 +48,7 @@ resource "aws_iam_role_policy" "lambda_secrets_policy" {
         Effect   = "Allow"
         Resource = [
           aws_db_instance.postgres.master_user_secret[0].secret_arn,
-          aws_secretsmanager_secret.firebase_credentials.arn
+          data.aws_secretsmanager_secret.firebase_credentials.arn
         ]
       }
     ]
