52250d89f9585e4aa152901ccf1420e1cdaa4d39
Ashley <ashleydai2003@gmail.com>
2025-05-26 20:46:49 -0700

    Schema tweaks and infra update

--- Infra/s3.tf
+++ Infra/s3.tf
@@ -23,3 +23,49 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "user_images_encry
   }
 }
 
+resource "aws_s3_bucket" "cove_images" {
+  bucket = "cove-cove-images"
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_versioning" "cove_images_versioning" {
+  bucket = aws_s3_bucket.cove_images.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cove_images_encryption" {
+  bucket = aws_s3_bucket.cove_images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "event_images" {
+  bucket = "cove-event-images"
+
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_versioning" "event_images_versioning" {
+  bucket = aws_s3_bucket.event_images.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "event_images_encryption" {
+  bucket = aws_s3_bucket.event_images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
