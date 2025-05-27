3187f53d4bf1359ba4814b981c50dde4ccf35b2a
Ashley <ashleydai2003@gmail.com>
2025-05-26 23:28:35 -0700

    Friend Request API and front end logic

--- CoveApp/Views/Onboarding/Flows/OnboardingFlow.swift
+++ CoveApp/Views/Onboarding/Flows/OnboardingFlow.swift
@@ -11,7 +11,7 @@ struct OnboardingFlow: View {
 
     var body: some View {
         NavigationStack(path: $appController.path) {
-            NamePageView()
+            MutualsView()
                 .navigationDestination(for: OnboardingRoute.self) { route in
                     switch route {
                     case .enterPhoneNumber:
