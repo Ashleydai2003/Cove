b9f10572fe225d9324f9889857d32685e0676439
Ashley <ashleydai2003@gmail.com>
2025-07-02 02:13:33 +0800

    test

--- Backend/src/routes/contacts.ts
+++ Backend/src/routes/contacts.ts
@@ -24,6 +24,8 @@ const s3Client = new S3Client({ region: process.env.AWS_REGION });
  */
 export const handleContacts = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
+    // TODO: temp, remove after testing
+    console.log("Test CI");
     // Check if the request method is POST
     if (event.httpMethod !== 'POST') {
       return {
